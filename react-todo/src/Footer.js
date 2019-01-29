ec8bba8a9e13497341b90641a143d0d0c710e6cf
aoife kavanagh <nyhankaa@tcd.ie>
2019-01-29 16:27:33 +0000

    added components for header, main & footer

---
+++ react-todo/src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer(){
+    return(
+      <div class="footer-copyright">
+            <div class="container">
+                © 2014 Copyright Text
+                <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+      </div>
+    );
+}
+
+export default Footer
