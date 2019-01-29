aa596fdc0a93df9c2432b4b78915a0b4d06cb407
aoife kavanagh <nyhankaa@tcd.ie>
2019-01-29 16:28:32 +0000

    added header, footer & main components to App component

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -3,10 +3,10 @@ import React from 'react';
 function App(){
   return(
     <div>
-      <input type='checkbox' name='item1' value='item1'/><p>Item 1</p>
-      <input type='checkbox' name='item2' value='item2'/><p>Item 2</p>
-      <input type='checkbox' name='item3' value='item3'/><p>Item 3</p>
-    </div> 
+      <Header />
+      <Main />
+      <Footer />
+    </div>
   );
 }
 
