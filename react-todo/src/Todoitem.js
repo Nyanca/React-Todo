716fe73994c5086bdc04dd91d6fec757f5c895f7
aoife kavanagh <nyhankaa@tcd.ie>
2019-01-29 17:21:45 +0000

    separated todo items into seperate component

---
+++ react-todo/src/Todoitem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Todoitem(){
+    return(
+        <p>
+          <label>
+            <input type="checkbox"/>
+            <span>Yellow</span>
+          </label>
+        </p>   
+    )
+}
+
+export default Todoitem
